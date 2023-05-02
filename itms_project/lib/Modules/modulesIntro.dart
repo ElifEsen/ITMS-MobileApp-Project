57fd95b8ffaf5fde78a1be4a4558504182e70fdd
ElifEsen <esnelif23@gmail.com>
2023-05-02 20:54:15 +0300

    Update modulesIntro.dart

--- itms_project/lib/Modules/modulesIntro.dart
+++ itms_project/lib/Modules/modulesIntro.dart
@@ -7,11 +7,53 @@ class modulesIntro extends StatelessWidget {
       body: Center(
         child: Container(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[],
+            children: <Widget>[
+              SizedBox(
+                height: 50.0,
+              ),
+              Text(
+                  "ITIL 4 Modules are Guiding Principles, Governance, Service Value Chain, Continual Improvement, Practices. You can select below to get information about each."),
+              SizedBox(
+                height: 20.0,
+              ),
+              InkWell(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (BuildContext context) =>
+                                guidingPrinciples()));
+                  },
+                  child: Text("Guiding Principles")),
+              SizedBox(
+                height: 20.0,
+              ),
+              Text("Governance"),
+              SizedBox(
+                height: 20.0,
+              ),
+              Text("Service Value Chain"),
+              SizedBox(
+                height: 20.0,
+              ),
+              Text("Continual Improvement"),
+              SizedBox(
+                height: 20.0,
+              ),
+              Text("Practices"),
+            ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class guidingPrinciples extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(child: Text("deneme")),
     );
   }
 }
