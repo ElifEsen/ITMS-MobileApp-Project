2656a43e869d6d64dfa5e0979c0f8c23f6015f6b
ElifEsen <esnelif23@gmail.com>
2023-05-02 16:10:43 +0300

    secondaryPage

--- itms_project/lib/secondaryPage.dart
+++ itms_project/lib/secondaryPage.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:itms_project/Modules/modulesIntro.dart';
+import 'package:itms_project/Processes/processesIntro.dart';
 
 class secondaryPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.blue,
       body: Center(
         child: Container(
           child: Column(
@@ -17,14 +19,42 @@ class secondaryPage extends StatelessWidget {
                         MaterialPageRoute(
                             builder: (BuildContext context) => modulesIntro()));
                   },
-                  child: Text(
-                    "ITIL 4 Modules",
-                    style: TextStyle(
-                        fontSize: 30.0, backgroundColor: Colors.white),
+                  child: Container(
+                    alignment: Alignment.center,
+                    width: MediaQuery.of(context).size.width - 50,
+                    height: MediaQuery.of(context).size.height / 8,
+                    decoration: BoxDecoration(
+                      color: Colors.blueGrey,
+                      borderRadius: BorderRadiusDirectional.circular(20.0),
+                    ),
+                    child: Text(
+                      "ITIL 4 Modules",
+                      style: TextStyle(fontSize: 30.0),
+                    ),
                   )),
-              Text(
-                "ITIL 4 Processes",
-                style: TextStyle(fontSize: 30.0, backgroundColor: Colors.white),
+              SizedBox(
+                height: 35.0,
+              ),
+              InkWell(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (BuildContext context) => processesIntro()));
+                },
+                child: Container(
+                  alignment: Alignment.center,
+                  width: MediaQuery.of(context).size.width - 50,
+                  height: MediaQuery.of(context).size.height / 8,
+                  decoration: BoxDecoration(
+                    color: Colors.blueGrey,
+                    borderRadius: BorderRadiusDirectional.circular(20.0),
+                  ),
+                  child: Text(
+                    "ITIL 4 Processes",
+                    style: TextStyle(fontSize: 30.0),
+                  ),
+                ),
               )
             ],
           ),
