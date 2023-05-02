c12b3f976b34b909670bd7778ede3ca242ebcb25
ElifEsen <esnelif23@gmail.com>
2023-05-02 23:42:49 +0300

    update modules pages and processes pages

--- itms_project/lib/Modules/modulesIntro.dart
+++ itms_project/lib/Modules/modulesIntro.dart
@@ -1,20 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:itms_project/Modules/continualImprovement.dart';
+import 'package:itms_project/Modules/governance.dart';
+import 'package:itms_project/Modules/guidingPrinciples.dart';
+import 'package:itms_project/Modules/practices.dart';
+import 'package:itms_project/Modules/svc.dart';
 
 class modulesIntro extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        actions: [
+          Padding(
+              padding: const EdgeInsets.only(right: 1.0),
+              child: Image.asset(
+                "assets/iucMuhendislikLogo.png",
+                fit: BoxFit.cover,
+              ))
+        ],
+      ),
+      backgroundColor: Colors.blue,
       body: Center(
         child: Container(
+          color: Color.fromARGB(200, 33, 96, 243),
+          margin: EdgeInsets.all(10.0),
           child: Column(
             children: <Widget>[
               SizedBox(
-                height: 50.0,
-              ),
-              Text(
-                  "ITIL 4 Modules are Guiding Principles, Governance, Service Value Chain, Continual Improvement, Practices. You can select below to get information about each."),
-              SizedBox(
                 height: 20.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Text(
+                  "ITIL 4 Modules are Guiding Principles, Governance, Service Value Chain, Continual Improvement, Practices. You can select below to get information about each.",
+                  style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.justify,
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
               ),
               InkWell(
                   onTap: () {
@@ -24,36 +49,117 @@ class modulesIntro extends StatelessWidget {
                             builder: (BuildContext context) =>
                                 guidingPrinciples()));
                   },
-                  child: Text("Guiding Principles")),
+                  child: Container(
+                      alignment: Alignment.center,
+                      width: MediaQuery.of(context).size.width - 180.0,
+                      height: MediaQuery.of(context).size.height / 11,
+                      decoration: BoxDecoration(
+                        color: Colors.blueGrey,
+                        borderRadius: BorderRadiusDirectional.circular(20.0),
+                      ),
+                      child: Text(
+                        "Guiding Principles",
+                        style: TextStyle(
+                            fontSize: 15.0, fontWeight: FontWeight.w600),
+                      ))),
               SizedBox(
-                height: 20.0,
+                height: 10.0,
               ),
-              Text("Governance"),
+              InkWell(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (BuildContext context) => governance()));
+                  },
+                  child: Container(
+                      alignment: Alignment.center,
+                      width: MediaQuery.of(context).size.width - 180.0,
+                      height: MediaQuery.of(context).size.height / 11,
+                      decoration: BoxDecoration(
+                        color: Colors.blueGrey,
+                        borderRadius: BorderRadiusDirectional.circular(20.0),
+                      ),
+                      child: Text(
+                        "Governance",
+                        style: TextStyle(
+                            fontSize: 15.0, fontWeight: FontWeight.w600),
+                      ))),
               SizedBox(
-                height: 20.0,
+                height: 10.0,
               ),
-              Text("Service Value Chain"),
+              InkWell(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (BuildContext context) =>
+                                serviceValueChain()));
+                  },
+                  child: Container(
+                      alignment: Alignment.center,
+                      width: MediaQuery.of(context).size.width - 180.0,
+                      height: MediaQuery.of(context).size.height / 11,
+                      decoration: BoxDecoration(
+                        color: Colors.blueGrey,
+                        borderRadius: BorderRadiusDirectional.circular(20.0),
+                      ),
+                      child: Text(
+                        "Service Value Chain",
+                        style: TextStyle(
+                            fontSize: 15.0, fontWeight: FontWeight.w600),
+                      ))),
               SizedBox(
-                height: 20.0,
+                height: 10.0,
               ),
-              Text("Continual Improvement"),
+              InkWell(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (BuildContext context) =>
+                                continualImprovement()));
+                  },
+                  child: Container(
+                      alignment: Alignment.center,
+                      width: MediaQuery.of(context).size.width - 180.0,
+                      height: MediaQuery.of(context).size.height / 11,
+                      decoration: BoxDecoration(
+                        color: Colors.blueGrey,
+                        borderRadius: BorderRadiusDirectional.circular(20.0),
+                      ),
+                      child: Text(
+                        "Continual Improvement",
+                        style: TextStyle(
+                            fontSize: 15.0, fontWeight: FontWeight.w600),
+                      ))),
               SizedBox(
-                height: 20.0,
+                height: 10.0,
               ),
-              Text("Practices"),
+              InkWell(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (BuildContext context) => practices()));
+                  },
+                  child: Container(
+                      alignment: Alignment.center,
+                      width: MediaQuery.of(context).size.width - 180.0,
+                      height: MediaQuery.of(context).size.height / 11,
+                      decoration: BoxDecoration(
+                        color: Colors.blueGrey,
+                        borderRadius: BorderRadiusDirectional.circular(20.0),
+                      ),
+                      child: Text(
+                        "Practices",
+                        style: TextStyle(
+                            fontSize: 15.0, fontWeight: FontWeight.w600),
+                      ))),
             ],
           ),
         ),
       ),
-    );
-  }
-}
-
-class guidingPrinciples extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Text("deneme")),
     );
   }
 }
--- itms_project/lib/Processes/processesIntro.dart
+++ itms_project/lib/Processes/processesIntro.dart
@@ -1,14 +1,117 @@
 import 'package:flutter/material.dart';
+import 'package:itms_project/Processes/general.dart';
+import 'package:itms_project/Processes/service.dart';
+import 'package:itms_project/Processes/technical.dart';
 
 class processesIntro extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        actions: [
+          Padding(
+              padding: const EdgeInsets.only(right: 1.0),
+              child: Image.asset(
+                "assets/iucMuhendislikLogo.png",
+                fit: BoxFit.cover,
+              ))
+        ],
+      ),
+      backgroundColor: Colors.blue,
       body: Center(
         child: Container(
+          color: Color.fromARGB(200, 33, 96, 243),
+          margin: EdgeInsets.all(10.0),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[],
+            children: <Widget>[
+              SizedBox(
+                height: 20.0,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Text(
+                  "ITIL 4 processes are a set of practices that help organizations to deliver value to their customers through effective service management. The ITIL 4 framework includes 34 management practices that are grouped into three categories: general management practices, service management practices, and technical management practices. You can select below to get information about each.",
+                  style: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.justify,
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              InkWell(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (BuildContext context) =>
+                                generalManagement()));
+                  },
+                  child: Container(
+                    alignment: Alignment.center,
+                    width: MediaQuery.of(context).size.width - 100.0,
+                    height: MediaQuery.of(context).size.height / 11,
+                    decoration: BoxDecoration(
+                      color: Colors.blueGrey,
+                      borderRadius: BorderRadiusDirectional.circular(20.0),
+                    ),
+                    child: Text(
+                      "General Management Processes",
+                      style: TextStyle(
+                          fontSize: 15.0, fontWeight: FontWeight.w600),
+                    ),
+                  )),
+              SizedBox(
+                height: 10.0,
+              ),
+              InkWell(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (BuildContext context) =>
+                                serviceManagement()));
+                  },
+                  child: Container(
+                    alignment: Alignment.center,
+                    width: MediaQuery.of(context).size.width - 100.0,
+                    height: MediaQuery.of(context).size.height / 11,
+                    decoration: BoxDecoration(
+                      color: Colors.blueGrey,
+                      borderRadius: BorderRadiusDirectional.circular(20.0),
+                    ),
+                    child: Text(
+                      "Service Management Processes",
+                      style: TextStyle(
+                          fontSize: 15.0, fontWeight: FontWeight.w600),
+                    ),
+                  )),
+              SizedBox(
+                height: 10.0,
+              ),
+              InkWell(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (BuildContext context) =>
+                                technicalManagement()));
+                  },
+                  child: Container(
+                    alignment: Alignment.center,
+                    width: MediaQuery.of(context).size.width - 100.0,
+                    height: MediaQuery.of(context).size.height / 11,
+                    decoration: BoxDecoration(
+                      color: Colors.blueGrey,
+                      borderRadius: BorderRadiusDirectional.circular(20.0),
+                    ),
+                    child: Text(
+                      "Technical Management Processes",
+                      style: TextStyle(
+                          fontSize: 15.0, fontWeight: FontWeight.w600),
+                    ),
+                  ))
+            ],
           ),
         ),
       ),
