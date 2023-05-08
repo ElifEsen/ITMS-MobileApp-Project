5c67d671d77ecc36b25668c2406f3e6d826f500c
ElifEsen <esnelif23@gmail.com>
2023-05-08 18:43:01 +0300

    Update guidingPrinciples.dart

--- itms_project/lib/Modules/guidingPrinciples.dart
+++ itms_project/lib/Modules/guidingPrinciples.dart
@@ -15,184 +15,188 @@ class guidingPrinciples extends StatelessWidget {
         backgroundColor: Colors.blue,
       ),
       backgroundColor: Colors.blue,
-      body: Center(
-        child: Container(
-          child: Column(
-            children: <Widget>[
-              SizedBox(
-                height: 5.0,
-              ),
-              Container(
-                padding: EdgeInsets.all(8.0),
-                child: Text(
-                  "A guiding principle is a recommendation that guides an organization in all circumstances, regardless of changes in its goals, strategies, type of work, or management structure. A guiding principle is universal and enduring. You can look at overview of it with the following:",
-                  textAlign: TextAlign.justify,
-                  style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+      body: ListView(
+        children: <Widget>[
+          Container(
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 50.0,
                 ),
-              ),
-              SizedBox(
-                height: 10.0,
-              ),
-              InkWell(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (BuildContext context) => focusOnValue()));
-                  },
-                  child: Container(
-                      alignment: Alignment.center,
-                      width: MediaQuery.of(context).size.width - 80.0,
-                      height: MediaQuery.of(context).size.height / 14,
-                      decoration: BoxDecoration(
-                        color: Colors.blueGrey,
-                        borderRadius: BorderRadiusDirectional.circular(20.0),
-                      ),
-                      child: Text(
-                        "Focus On Value",
-                        style: TextStyle(fontSize: 16.0),
-                      ))),
-              SizedBox(
-                height: 10.0,
-              ),
-              InkWell(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (BuildContext context) =>
-                                startWhereYouAre()));
-                  },
-                  child: Container(
-                      alignment: Alignment.center,
-                      width: MediaQuery.of(context).size.width - 80.0,
-                      height: MediaQuery.of(context).size.height / 14,
-                      decoration: BoxDecoration(
-                        color: Colors.blueGrey,
-                        borderRadius: BorderRadiusDirectional.circular(20.0),
-                      ),
-                      child: Text(
-                        "Start Where You Are",
-                        style: TextStyle(fontSize: 16.0),
-                      ))),
-              SizedBox(
-                height: 10.0,
-              ),
-              InkWell(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (BuildContext context) =>
-                                progressIterativelyWithFeedback()));
-                  },
-                  child: Container(
-                      alignment: Alignment.center,
-                      width: MediaQuery.of(context).size.width - 80.0,
-                      height: MediaQuery.of(context).size.height / 14,
-                      decoration: BoxDecoration(
-                        color: Colors.blueGrey,
-                        borderRadius: BorderRadiusDirectional.circular(20.0),
-                      ),
-                      child: Text(
-                        "Progress Iteratively with Feedback",
-                        style: TextStyle(fontSize: 16.0),
-                      ))),
-              SizedBox(
-                height: 10.0,
-              ),
-              InkWell(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (BuildContext context) =>
-                                collaborateAndPromoteVisibility()));
-                  },
-                  child: Container(
-                      alignment: Alignment.center,
-                      width: MediaQuery.of(context).size.width - 80.0,
-                      height: MediaQuery.of(context).size.height / 14,
-                      decoration: BoxDecoration(
-                        color: Colors.blueGrey,
-                        borderRadius: BorderRadiusDirectional.circular(20.0),
-                      ),
-                      child: Text(
-                        "Collaborate and Promote Visibility",
-                        style: TextStyle(fontSize: 16.0),
-                      ))),
-              SizedBox(
-                height: 10.0,
-              ),
-              InkWell(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (BuildContext context) =>
-                                thinkAndWorkHolistically()));
-                  },
-                  child: Container(
-                      alignment: Alignment.center,
-                      width: MediaQuery.of(context).size.width - 80.0,
-                      height: MediaQuery.of(context).size.height / 14,
-                      decoration: BoxDecoration(
-                        color: Colors.blueGrey,
-                        borderRadius: BorderRadiusDirectional.circular(20.0),
-                      ),
-                      child: Text(
-                        "Think and Work Holistically",
-                        style: TextStyle(fontSize: 16.0),
-                      ))),
-              SizedBox(
-                height: 10.0,
-              ),
-              InkWell(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (BuildContext context) =>
-                                keepItSimpleAndPractical()));
-                  },
-                  child: Container(
-                      alignment: Alignment.center,
-                      width: MediaQuery.of(context).size.width - 80.0,
-                      height: MediaQuery.of(context).size.height / 14,
-                      decoration: BoxDecoration(
-                        color: Colors.blueGrey,
-                        borderRadius: BorderRadiusDirectional.circular(20.0),
-                      ),
-                      child: Text(
-                        "Keep It Simple and Practical",
-                        style: TextStyle(fontSize: 16.0),
-                      ))),
-              SizedBox(
-                height: 10.0,
-              ),
-              InkWell(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (BuildContext context) =>
-                                optimizeAndAutomate()));
-                  },
-                  child: Container(
-                      alignment: Alignment.center,
-                      width: MediaQuery.of(context).size.width - 80.0,
-                      height: MediaQuery.of(context).size.height / 14,
-                      decoration: BoxDecoration(
-                        color: Colors.blueGrey,
-                        borderRadius: BorderRadiusDirectional.circular(20.0),
-                      ),
-                      child: Text(
-                        "Optimize and Automate",
-                        style: TextStyle(fontSize: 16.0),
-                      ))),
-            ],
+                Container(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    "A guiding principle is a recommendation that guides an organization in all circumstances, regardless of changes in its goals, strategies, type of work, or management structure. A guiding principle is universal and enduring. You can look at overview of it with the following:",
+                    textAlign: TextAlign.justify,
+                    style:
+                        TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                InkWell(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (BuildContext context) =>
+                                  focusOnValue()));
+                    },
+                    child: Container(
+                        alignment: Alignment.center,
+                        width: MediaQuery.of(context).size.width - 80.0,
+                        height: MediaQuery.of(context).size.height / 14,
+                        decoration: BoxDecoration(
+                          color: Colors.blueGrey,
+                          borderRadius: BorderRadiusDirectional.circular(20.0),
+                        ),
+                        child: Text(
+                          "Focus On Value",
+                          style: TextStyle(fontSize: 16.0),
+                        ))),
+                SizedBox(
+                  height: 10.0,
+                ),
+                InkWell(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (BuildContext context) =>
+                                  startWhereYouAre()));
+                    },
+                    child: Container(
+                        alignment: Alignment.center,
+                        width: MediaQuery.of(context).size.width - 80.0,
+                        height: MediaQuery.of(context).size.height / 14,
+                        decoration: BoxDecoration(
+                          color: Colors.blueGrey,
+                          borderRadius: BorderRadiusDirectional.circular(20.0),
+                        ),
+                        child: Text(
+                          "Start Where You Are",
+                          style: TextStyle(fontSize: 16.0),
+                        ))),
+                SizedBox(
+                  height: 10.0,
+                ),
+                InkWell(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (BuildContext context) =>
+                                  progressIterativelyWithFeedback()));
+                    },
+                    child: Container(
+                        alignment: Alignment.center,
+                        width: MediaQuery.of(context).size.width - 80.0,
+                        height: MediaQuery.of(context).size.height / 14,
+                        decoration: BoxDecoration(
+                          color: Colors.blueGrey,
+                          borderRadius: BorderRadiusDirectional.circular(20.0),
+                        ),
+                        child: Text(
+                          "Progress Iteratively with Feedback",
+                          style: TextStyle(fontSize: 16.0),
+                        ))),
+                SizedBox(
+                  height: 10.0,
+                ),
+                InkWell(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (BuildContext context) =>
+                                  collaborateAndPromoteVisibility()));
+                    },
+                    child: Container(
+                        alignment: Alignment.center,
+                        width: MediaQuery.of(context).size.width - 80.0,
+                        height: MediaQuery.of(context).size.height / 14,
+                        decoration: BoxDecoration(
+                          color: Colors.blueGrey,
+                          borderRadius: BorderRadiusDirectional.circular(20.0),
+                        ),
+                        child: Text(
+                          "Collaborate and Promote Visibility",
+                          style: TextStyle(fontSize: 16.0),
+                        ))),
+                SizedBox(
+                  height: 10.0,
+                ),
+                InkWell(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (BuildContext context) =>
+                                  thinkAndWorkHolistically()));
+                    },
+                    child: Container(
+                        alignment: Alignment.center,
+                        width: MediaQuery.of(context).size.width - 80.0,
+                        height: MediaQuery.of(context).size.height / 14,
+                        decoration: BoxDecoration(
+                          color: Colors.blueGrey,
+                          borderRadius: BorderRadiusDirectional.circular(20.0),
+                        ),
+                        child: Text(
+                          "Think and Work Holistically",
+                          style: TextStyle(fontSize: 16.0),
+                        ))),
+                SizedBox(
+                  height: 10.0,
+                ),
+                InkWell(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (BuildContext context) =>
+                                  keepItSimpleAndPractical()));
+                    },
+                    child: Container(
+                        alignment: Alignment.center,
+                        width: MediaQuery.of(context).size.width - 80.0,
+                        height: MediaQuery.of(context).size.height / 14,
+                        decoration: BoxDecoration(
+                          color: Colors.blueGrey,
+                          borderRadius: BorderRadiusDirectional.circular(20.0),
+                        ),
+                        child: Text(
+                          "Keep It Simple and Practical",
+                          style: TextStyle(fontSize: 16.0),
+                        ))),
+                SizedBox(
+                  height: 10.0,
+                ),
+                InkWell(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (BuildContext context) =>
+                                  optimizeAndAutomate()));
+                    },
+                    child: Container(
+                        alignment: Alignment.center,
+                        width: MediaQuery.of(context).size.width - 80.0,
+                        height: MediaQuery.of(context).size.height / 14,
+                        decoration: BoxDecoration(
+                          color: Colors.blueGrey,
+                          borderRadius: BorderRadiusDirectional.circular(20.0),
+                        ),
+                        child: Text(
+                          "Optimize and Automate",
+                          style: TextStyle(fontSize: 16.0),
+                        ))),
+              ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
