0fdc9dfa32406b7e8c8ceea2c1e27bbddf2f3358
ElifEsen <esnelif23@gmail.com>
2023-05-09 01:09:11 +0300

    Update practices.dart

--- itms_project/lib/Modules/practices.dart
+++ itms_project/lib/Modules/practices.dart
@@ -16,7 +16,7 @@ class practices extends StatelessWidget {
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Text(
-                  "A practice is a set of organizational resources designed for performing work or accomplishing an objective. These resources are grouped into the four dimensions of service management. The ITIL SVS includes general management, service management, and technical management practices, as described in 'ITIL Practices' part.",
+                  "A practice is a set of organizational resources designed for performing work or accomplishing an objective. These resources are grouped into the four dimensions of service management. The ITIL SVS includes general management, service management, and technical management practices, as described in 'ITIL Processes' part.",
                   textAlign: TextAlign.justify,
                   style: TextStyle(fontSize: 16.0),
                 ),
