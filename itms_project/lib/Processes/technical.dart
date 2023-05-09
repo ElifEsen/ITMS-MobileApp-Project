96f7dd86b286f9ee35dd91d3b8ccd901fecb7141
ElifEsen <esnelif23@gmail.com>
2023-05-10 01:35:16 +0300

    Update technicalManagementPractices

--- itms_project/lib/Processes/technical.dart
+++ itms_project/lib/Processes/technical.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:itms_project/Processes/technicalManagement/deploymentManagement.dart';
+import 'package:itms_project/Processes/technicalManagement/infrastructureAndPlatformManagement.dart';
+import 'package:itms_project/Processes/technicalManagement/softwareDevAndManagement.dart';
 
 class technicalManagement extends StatelessWidget {
   @override
@@ -8,14 +11,89 @@ class technicalManagement extends StatelessWidget {
         backgroundColor: Colors.blue,
       ),
       backgroundColor: Colors.blue,
-      body: Center(
-        child: Container(
+      body: ListView(children: <Widget>[
+        Container(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[Text("deneme")],
+            children: <Widget>[
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                "deneme",
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              InkWell(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (BuildContext context) =>
+                                deploymentManagement()));
+                  },
+                  child: Container(
+                      alignment: Alignment.center,
+                      width: MediaQuery.of(context).size.width - 80.0,
+                      height: MediaQuery.of(context).size.height / 14,
+                      decoration: BoxDecoration(
+                        color: Colors.blueGrey,
+                        borderRadius: BorderRadiusDirectional.circular(20.0),
+                      ),
+                      child: Text(
+                        "Deployment Management",
+                        style: TextStyle(fontSize: 16.0),
+                      ))),
+              SizedBox(
+                height: 10.0,
+              ),
+              InkWell(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (BuildContext context) =>
+                                infrastructureAndPlatformManagement()));
+                  },
+                  child: Container(
+                      alignment: Alignment.center,
+                      width: MediaQuery.of(context).size.width - 80.0,
+                      height: MediaQuery.of(context).size.height / 14,
+                      decoration: BoxDecoration(
+                        color: Colors.blueGrey,
+                        borderRadius: BorderRadiusDirectional.circular(20.0),
+                      ),
+                      child: Text(
+                        "Infrastructure & Platform Management",
+                        style: TextStyle(fontSize: 16.0),
+                      ))),
+              SizedBox(
+                height: 10.0,
+              ),
+              InkWell(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (BuildContext context) =>
+                                softwareDevelopmentAndManagement()));
+                  },
+                  child: Container(
+                      alignment: Alignment.center,
+                      width: MediaQuery.of(context).size.width - 80.0,
+                      height: MediaQuery.of(context).size.height / 14,
+                      decoration: BoxDecoration(
+                        color: Colors.blueGrey,
+                        borderRadius: BorderRadiusDirectional.circular(20.0),
+                      ),
+                      child: Text(
+                        "Software Development & Management",
+                        style: TextStyle(fontSize: 16.0),
+                      ))),
+            ],
           ),
         ),
-      ),
+      ]),
     );
   }
 }
